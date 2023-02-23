101f0846eb8f7d708c1db558dc2618c85b6fcc7b
Le Pham <phamtle724@gmail.com>
2023-02-22 19:34:23 -0800

    center displayWeather

--- weather-frontend/src/displayWeather/DisplayWeather.js
+++ weather-frontend/src/displayWeather/DisplayWeather.js
@@ -1,9 +1,9 @@
 import { Navigate, useNavigate } from "react-router-dom";
-
+import "./DisplayWeather.css"
 function DisplayWeather({suggestion}) {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className="suggestion">
       {suggestion}
       <button onClick={()=> navigate("/where-thee")}
 > locate me again please</button>
